3e49c6ecde6337ef73e5c97d92dabb031ffda9ce
Evelina Nuñez <enunez@Garantizar.com.ar>
2023-10-01 19:07:11 -0300

    actualizo desafio Forms Reactive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,92 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup ,Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms-alumno',
+  templateUrl: './reactive-forms.component.html',
+  styleUrls: ['./reactive-forms.component.css']
+})
+export class ReactiveFormsComponent {
+
+  alumnoForm : FormGroup;
+
+  constructor( private formBuilder : FormBuilder){
+    this.alumnoForm = this.formBuilder.group({
+      /**
+       * Forma de hacerlo mas corto, con un array
+       * Ej: nombreAlumno : [ valorPorDefecto]
+       */
+      nombreAlumno: ['', [Validators.required, Validators.maxLength(50),Validators.minLength(7)]],
+      apellidoAlumno :['',[Validators.required,Validators.minLength(5)]],
+      fechaNacimiento: ['',[Validators.required]],
+      emailform:['',[Validators.required, Validators.email, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]],
+      //emailform:['',[Validators.required, Validators.email]],
+      genero: ['',[Validators.required]],
+      direccion : ['',[Validators.required]],
+      telefono : ['',[Validators.required]],
+      /* nombreAlumno : this.formBuilder.control('Evelina'),
+      apellidoAlumno :this.formBuilder.control('Nuñez'),
+      fechaNacimiento: this.formBuilder.control('1989-11-28'),
+      genero: this.formBuilder.control(''),
+      direccion : this.formBuilder.control(''),
+      telefono : this.formBuilder.control('1223333'), */
+    });
+
+
+  }
+  /**
+   * Metodo que se ejecuta cuando se acciona el ngSubmit del formulario
+   */
+  onSubmit(){
+
+/*     console.log( this.alumnoForm.valid);
+    console.log(this.alumnoForm); */
+    console.log(this.alumnoForm.value);
+    // console.log(this.alumnoForm.controls['nombreAlumno'].value);
+  }
+
+  get nombreAlumnoControl(){
+    return this.alumnoForm.controls['nombreAlumno'];
+  }
+
+  get apellidoAlumnoControl(){
+    return this.alumnoForm.controls['apellidoAlumno'];
+  }
+
+
+  get fechaNacimientoControl(){
+    return this.alumnoForm.controls['fechaNacimiento'];
+  }
+
+
+  get emailformControl(){
+    return this.alumnoForm.controls['emailform'];
+  }
+
+  get generoControl(){
+    return this.alumnoForm.controls['genero'];
+  }
+
+  get direccionControl(){
+    return this.alumnoForm.controls['direccion'];
+  }
+
+  get telefonoControl(){
+    return this.alumnoForm.controls['telefono'];
+  }
+
+  /**
+   * Forma de acceder al objeto controls.
+   *   console.log( this.alumnoForm.valid);
+       console.log(this.alumnoForm);
+       console.log(this.alumnoForm.value);
+       console.log(this.alumnoForm.controls['nombreAlumno'].value);
+   *
+   */
+  /**
+   * Validaciones: clase Validators
+   * this.alumnoForm.valid nos devuelve un true o false,
+   * dependiendo el estado del formulario
+   */
+}
+
